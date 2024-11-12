aed560782eef7c83fd0e51815ed5dde163568b67
dothikhanhhoa2003 <Khanhhoa16052003@gmail.com>
2024-11-12 11:22:16 +0700

    create_connection

--- bank.py
+++ bank.py
@@ -2,3 +2,7 @@ import tkinter as tk
 from tkinter import simpledialog, messagebox
 import sqlite3
 from datetime import datetime
+
+def create_connection():
+    conn = sqlite.connect("bank_accounts.db")
+    return conn
